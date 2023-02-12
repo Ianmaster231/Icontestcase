01137dd383e61568826e99c6d8dd991549402a42
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2023-02-12 13:02:09 +0100

    added the prices for the dimensional now going to test them i also realized i was working on the wrong project so i wasnt updating it by accident

--- icontestcasec#/icontestcase/Program.cs
+++ icontestcasec#/icontestcase/Program.cs
@@ -11,31 +11,144 @@ namespace icontestcase
         static void Main(string[] args)
         {
 
-            int eur10 = 999;
-            int[] eur20 = Enumerable.Range(1000, 2000).ToArray();
+            //console showing the user the three options that are avaliable as a mailing service
+            Console.WriteLine("Please select a mail order option:");
+            Console.WriteLine("1. Cargo4You 1");
+            Console.WriteLine("2. ShipFaster 2");
+            Console.WriteLine("3. MaltaShip 3");
+            Console.WriteLine("Enter your selection (1 or 2 or 3):");
+            //Storing the variable answer to be excituted 
+            int selection = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("Enter the weight :");
+            int input = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the width: ");
-            int a = Convert.ToInt32(Console.ReadLine());
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the height: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the depth: ");
+            int depth = Convert.ToInt32(Console.ReadLine());
 
-            /*    Console.WriteLine("Enter the Second Number: ");
-                int b = Convert.ToInt32(Console.ReadLine());
+            int cost = width + height + depth;
 
-                Console.WriteLine("Enter the Third Number: ");
-                int c = Convert.ToInt32(Console.ReadLine());
-
-                int large = (a > b) ? ((b > c) ? a : (c > a) ? c : a) : (b > c) ? b : c; */
-
-            
-            if(a <= eur10)
+            decimal weightCost;
+            decimal parcelSizeCost = cost;
+            switch (selection)
             {
-                Console.WriteLine("Parcel cost is 10 Eur");
+                case 1:
+                    weightCost = weightcostCargo4You(input);
+                    parcelSizeCost = parcelsizeMaltapost(cost);
+                    break;
+                case 2:
+                    weightCost = weightcostShipFaster(input);
+                    break;
+                case 3:
+                    weightCost = weightcostMaltapost(input);
+                    break;
+                default:
+                    Console.WriteLine("Invalid selection. Please try again.");
+                    return;
             }
-            else if (a > 1000 || a <= 2000)
+            if (weightCost >= parcelSizeCost)
             {
-                Console.WriteLine("Parcel cost is 20 Eur");
-            }    
+                Console.WriteLine("Parcel cost: " + weightCost);
+            }
+            else
+            {
+                Console.WriteLine("Parcel cost: " + parcelSizeCost);
+            }
+            //Console.WriteLine("Parcel cost: " + weightCost);
+            //Console.WriteLine("Parcel cost: " + parcelSizeCost);
 
-           
+        }
+
+        static decimal parcelsizeMaltapost(int cost)
+        {
+            decimal pclsize;
+            switch (cost)
+            {
+                case int n when n <= 1000:
+                    pclsize = 10;
+                    break;
+                case int n when n > 1000 && n <= 2000:
+                    pclsize = 20;
+                    break;
+                case int n when n <= 2000:
+                    Console.WriteLine("Parcel Dimension exceded");
+                    pclsize = 0;
+                    break;
+                default:
+                    pclsize = 0;
+                    break;
+            }
+            return pclsize;
+        }
+
+        static decimal weightcostMaltapost(int weight)
+        {
+            decimal wgtcst;
+            switch (weight)
+            {
+                case int n when n <= 0:
+                    wgtcst = 0;
+                    Console.WriteLine("Local Shipping mostly depends on dimentions of the parcel");
+                    break;
+                default:
+                    wgtcst = 0;
+                    break;
+            }
+            return wgtcst;
+        }
+
+        static decimal weightcostCargo4You(int weight)
+        {
+            decimal wgtcst;
+            switch (weight)
+            {
+                case int n when n <= 2000:
+                    wgtcst = 15;
+                    break;
+                case int n when n > 2000 && n <= 15000:
+                    wgtcst = 18;
+                    break;
+                case int n when n > 15000 && n <= 19999:
+                    wgtcst = 35;
+                    break;
+                case int n when n >= 20000:
+                    wgtcst = 0;
+                    Console.WriteLine("Weight exceded");
+                    break;
+                default:
+                    wgtcst = 0;
+                    break;
+            }
+            return wgtcst;
+        }
+
+        static decimal weightcostShipFaster(int weight)
+        {
+            decimal wgtcst;
+            switch (weight)
+            {
+                case int n when n >= 10000 && n <= 15000:
+
+                    wgtcst = 16.50m;
+                    break;
+                case int n when n > 15000 && n <= 25000:
+                    wgtcst = 36.50m;
+                    break;
+                case int n when n > 25000:
+                    wgtcst = 40 + (weight - 25) * 0.417m;
+                    break;
+                case int n when n < 10000 && n <= 30000:
+                    wgtcst = 0;
+                    Console.WriteLine("Weight is to little or excided the maximum limit");
+                    break;
+                default:
+                    wgtcst = 0;
+                    break;
+            }
+            return wgtcst;
         }
     }
 }
